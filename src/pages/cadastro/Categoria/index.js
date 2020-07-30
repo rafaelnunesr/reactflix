06b9df60961e41f1eb8362214e599d45eaad9dec
Rafael Nunes <rafaelrafssk@hotmail.com>
2020-07-29 23:24:20 -0300

    segundo dia imersao react

---
+++ src/pages/cadastro/Categoria/index.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField'
+
+function CadastroCategoria() {
+  const [categorias, setCategorias] = useState([]);
+
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: ''
+  }
+
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor
+    })
+  }
+
+  function handleChange(infosDoEvento){
+    const { getAttribute, value } = infosDoEvento.target
+    setValue(
+      getAttribute('name'), 
+      value
+    )
+  }
+
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria: { values.nome }</h1>
+
+      <form onSubmit={function handleSubmit(infosDoEvento){
+        infosDoEvento.preventDefault()
+        setCategorias([
+          ...categorias,
+          values
+        ]);
+
+        setValues(valoresIniciais);
+      }}>
+
+        <FormField 
+          label='Nome da Categoria'
+          type='text'
+          name='nome'
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField 
+          label='Descrição'
+          type='text'
+          name='descricao'
+          value={values.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField 
+          label='Cor'
+          type='color'
+          name='cor'
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria.nome}
+            </li>
+          )
+        })}
+      </ul>
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
+}
+
+export default CadastroCategoria;
